c7aad3e3fa9e49670daf384044b50519907d4852
prabhdeepsamra <psamra2@ucmerced.edu>
2018-04-17 21:26:28 -0700

    Initial commit

---
+++ outline173.m
@@ -0,0 +1,22 @@
+input image
+
+preprocessing steps
+
+convolution matrix loop
+    resize image
+    process resized image
+    send matrix to network function
+    
+   
+        network function (input matrix)
+          compare to the 10 detector matrices
+            compare to the hierachal detector matrices
+            return percentage fit values
+    
+    
+    if within threshold
+         highlight area on output image and output the number
+    
+    
+    
+    
